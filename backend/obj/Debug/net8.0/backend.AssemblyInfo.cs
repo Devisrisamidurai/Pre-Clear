671b01b2d94457ff44fa9afcf8957e92ae985217
Devisri <devisris.it2022@citchennai.net>
2025-12-12 10:00:42 +0530

    [fix]: updated shipment schema

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275c61cd625f6ebc5bc8b244741f044a419dbbc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c3115f65d18b070df5fdc9af170c26f879f8b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
